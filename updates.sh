1c61ef522f8804b9638b856ac63c774576cc613a
sjapanwala <saaim.japanwala125@gmail.com>
2024-11-24 02:22:03 -0700

    added rofi conf

--- updates.sh
+++ updates.sh
@@ -3,3 +3,5 @@ cp ~/.config/i3/config ~/dotfiles/i3/config
 cp ~/.config/polybar/config.ini ~/dotfiles/polybar/config.ini
 cp ~/.config/neofetch/config.conf ~/dotfiles/neofetch/config.conf
 cp ~/.config/wezterm/wezterm.lua ~/dotfiles/wezterm/wezterm.lua
+cp ~/.config/rofi/config.rasi ~/dotfiles/rofi/config.rasi
+cp ~/.config/nvim/ ~/dotfiles/nvim/  
